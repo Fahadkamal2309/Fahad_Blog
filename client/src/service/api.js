e8733f7243fa06974b32d87dc81e5329051a64ff
Mohammad Fahad Kamal <8112fahadkamal@gmail.com>
2026-04-28 13:25:36 +0530

    Fix CORS for deployed frontend

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from "../constants/config";
 import { getAccessToken,getType } from '../utils/common-utils';
-const API_URL = "https://fahad-blog-cxaf.onrender.com";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://your-backend.onrender.com"
+    : "http://localhost:8000";
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
